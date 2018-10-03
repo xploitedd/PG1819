389e9ad91d58b0fe04538c85c8667de967670587
Ricardo Margalhau <kryfor@outlook.com>
2018-10-03 20:54:26 +0100

    added some comments

--- Trabalho1/Dice.java
+++ Trabalho1/Dice.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Dice {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int pt = (int) (Math.random() * 6 + 1);
-        System.out.println("Primeiro dado = [" + pt + "]\nPrima enter");
-        s.nextLine();
-
+        
         /*
         * 0 <= random < 1
         * 0 <= 6 * random < 6
         * 1 <= 6 * random + 1 < 7
         */
+
+        int pt = (int) (Math.random() * 6 + 1);
+        System.out.println("Primeiro dado = [" + pt + "]\nPrima enter");
+        s.nextLine();
         int segundo = (int) (Math.random() * 6 + 1);
 
         String total = "Total ";
